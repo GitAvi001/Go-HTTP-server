7969faf737299a84c19f679d27f7ce60785ba1b2
unknown <avindu@wso2.com>
2024-11-21 14:48:25 +0530

    Server starting on port 8080 changed with Server starting in 8080

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
-
 import (
 	"fmt"
 	"log"
 	"net/http"
 )
+
 func main() {
 
-	fmt.Println("Listening to port 8080")
+	fmt.Println("Server running on port 8080")
 
-	log.Fatal(http.ListenAndServe(":8080",nil))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
