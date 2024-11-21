4627ad75db5f81bebba3586a9c984b0b8c03793c
unknown <avindu@wso2.com>
2024-11-21 14:45:00 +0530

    main.go updated with the HTTP ListenAndServe

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
-import("fmt") 
 
-func main(){
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+func main() {
 
+	fmt.Println("Listening to port 8080")
 
+	log.Fatal(http.ListenAndServe(":8080",nil))
 
 }
